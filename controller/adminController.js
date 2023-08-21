8499b0dad89243aee3c266fa093f524e90df1daf
sa011 <sk.sabit.287@gmail.com>
2023-08-21 11:11:33 +0600

    Bug Fix

--- controller/adminController.js
+++ controller/adminController.js
@@ -1,6 +1,7 @@
 const matchDatabase = require('../database/match');
 const eventDatabase = require('../database/event');
 const matchController = require('./matchController');
+const teamDatabase = require('../database/teamInfo');
 module.exports.login = async (request, response) => {
     if(request.user.role != 'admin'){
         request.logout((err) => {
@@ -28,15 +29,26 @@ module.exports.logout = async (request, response) => {
 
 
 module.exports.getMatchWeek = async (request, response) => {
-    const matchWeek = await matchDatabase.getMatchesByWeek(request.params.week);
+    var matchWeek = await matchDatabase.getMatchesByWeek(request.params.week);
     if(matchWeek == null){
         response.status(400).send("Match week not found");
     }else{
+        for(let i = 0; i < matchWeek.length; i++){
+            if(matchWeek[i].finished == 1){
+                const score = await eventDatabase.getScoreLine(matchWeek[i]);
+                matchWeek[i].score = `${score.home} - ${score.away}`;
+            }
+            matchWeek[i].home = (await teamDatabase.getTeamInfo(matchWeek[i].home)).name;
+            matchWeek[i].away = (await teamDatabase.getTeamInfo(matchWeek[i].away)).name;
+            
+        }
+        console.log(matchWeek);
         response.status(200).send(matchWeek);
     }
 };
 
 module.exports.simulateMatch = async (request, response) => {
+    console.log(request.params);
     const matchInfo = await matchDatabase.getMatchInfo(request.params.match_id);
     if(matchInfo == null){
         response.status(400).send("Match not found");
@@ -44,7 +56,11 @@ module.exports.simulateMatch = async (request, response) => {
         response.status(400).send("Match already finished");
     }else{
         //Match simulation here. pending
-        matchController.simulateMatch(matchInfo);
-        response.status(200).send("Match simulated");
+        matchController.simulateMatch(matchInfo).then(() => {
+            response.status(200).send("Match simulated");
+        }).catch((err) => {
+            console.log(err);
+            response.status(500).send("Internal Server Error");
+        });
     }
 }
